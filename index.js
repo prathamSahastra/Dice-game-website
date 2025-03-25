cadaef047d264d9cf3ea6f7f1d86cab38e887882
Prathamesh Sahastrabuddhe <100425816+prathamSahastra@users.noreply.github.com>
2025-03-25 11:18:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg1 = "./images/dice"+randomNumber1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImg1);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg2 = "./images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImg2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🏆 Player 1 🏆";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "🏆 Player 2 🏆";
+}
+else{
+    document.querySelector("h1").innerHTML = "🤝 Draw 🤝";
+}
